10fd650913dbbbbbe3c73c793aa0bf2587993ead
wilson604 <wilson.evangelista@iesb.edu.br>
2025-09-18 21:29:38 -0300

    interface postman

--- praticas/06-crud-pessoas/Pessoas.js
+++ praticas/06-crud-pessoas/Pessoas.js
@@ -37,6 +37,65 @@ router.get('/pessoas/:id', (req,res, next) => {
     res.json(pessoa)
 })
 
+//#criação
+// POST /pessoas 
 
+router.post('/pessoas', (req, res, next) => {
+  const { nome, cpf, email, dataNascimento } = req.body
+  //validando se os campos foram preeenchidos 
+  if(!nome || !cpf || !email || !dataNascimento){
+    return res.status(400).json({ error: "Nome, cpf, email e DataNascimento são obrigatórios!!!"})
+  }
+  //validar se o cpf ja foi cadastrado 
+  if(listaPessoas.some(pessoa => pessoa.cpf == cpf)){
+    return res.status(409).json({ error: "CPF já cadastrado!!!"})
+  }
+
+  const novaPessoa = {
+    id: Date.now(),
+    nome,
+    cpf,
+    email,
+    dataNascimento
+  }
+
+  listaPessoas.push(novaPessoa)
+  res.status(201).json({ message: "Pessoa cadastrada com sucesso", novaPessoa})
+})
+
+//#Atualização 
+// PUT ou PATH /pessoas/:id 
+router.put('/pessoas/:id', (req, res, next) => {
+  const id = req.params.id
+  const pessoa = listaPessoas.find(pessoa => pessoa.id == id)
+  // valido se a pessoa existe 
+  if(!pessoa){
+    return res.status(404).json({ error: "Pessoa não encontrada!!!"})
+  }
+  const { nome, email, dataNascimento } = req.body
+  if(!nome || !email || !dataNascimento){
+    return res.status(400).json({ error: "nome, email, dataNascimento são obrigatórios"})
+  }
+  // atualizo os dados da pessoa 
+  pessoa.nome = nome 
+  pessoa.email = email 
+  pessoa.dataNascimento = dataNascimento
+  // responde com os dados da pessoa atualizados 
+  res.json({ message: "Pessoa atulizada com sucesso!!!", pessoa })
+})
+
+// #Remoção 
+// DELETE /pessoas/:id
+router.delete('/pessoas/:id', (req, res, next) => {
+  const id = req.params.id
+  //
+  const pessoa = listaPessoas.find(pessoa => pessoa.id == id)
+  if (!pessoa) {
+    return res.status(404).json({error: "pessoa não encontrada!!!" })
+  }
+
+  listaPessoas = listaPessoas.filter(pessoa => pessoa.id != id)
+  res.json({ message: "Pessoa excluida com sucesso!!!"})
+})
 // exportar corretamente
 module.exports = router
